6af1c8a37024598cc6147a153ff86eda6104f176
rabermudezg@gmail.com <rabermudezg@gmail.com>
2026-01-07 16:59:46 -0500

    Initial commit: Kelly App v2 - React frontend + FastAPI backend

---
+++ IOS/foodie/foodieTests/foodieTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodieTests.swift
+//  foodieTests
+//
+//  Created by Rodrigo Bermudez on 12/28/25.
+//
+
+import Testing
+@testable import foodie
+
+struct foodieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
